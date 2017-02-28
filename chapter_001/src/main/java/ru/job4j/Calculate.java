4546cf91d15770b8c8f157a3cf6e4973d8672ca8
alexTheMist <alexey.aleshin@yahoo.com>
2017-02-28 22:51:17 +0300

    Added Maven checksyle plugin, added plugin config files. Fixed some typos found by checksyle.

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,14 +1,14 @@
 package ru.job4j;
 
 /**
-*Class Calculate for demo purposes
+*Class Calculate for demo purposes.
 *@author Alexey Aleshin
 *@since 28.02.2017
 *@version 2
 */
 public class Calculate {
 	/**
-	*Method main are using as a start poin of the program
+	*Method main are using as a start poin of the program.
 	*@param args Arguments passed from command line
 	*/
 	public static void main(String[] args) {
--- chapter_001/src/test/java/ru/job4j/package-info.java
+++ chapter_001/src/test/java/ru/job4j/package-info.java
@@ -1,5 +1,5 @@
 /**
-* Package ru.job4j for learning purposes
+* Package ru.job4j for learning purposes.
 *
 * @author Alexey Aleshin (mailto:al-al-m@yandex.ru)
 * @version $Id$
