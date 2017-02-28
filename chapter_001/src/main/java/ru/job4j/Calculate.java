11355389b0ac3a72ee3ad8df4e607f2bcc0b183f
alexTheMist <alexey.aleshin@yahoo.com>
2017-02-28 21:55:36 +0300

    Added test dependencies, test class, package-info.

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
 /**
-*Class Calculate for demo purposes 
+*Class Calculate for demo purposes
 *@author Alexey Aleshin
 *@since 28.02.2017
 *@version 2
@@ -12,6 +12,6 @@ public class Calculate {
 	*@param args Arguments passed from command line
 	*/
 	public static void main(String[] args) {
-		System.out.println("Hello Wrold!");
+		System.out.println("Hello World!");
 	}
 }
---
+++ chapter_001/src/test/java/ru/job4j/package-info.java
@@ -0,0 +1,9 @@
+/**
+* Package ru.job4j for learning purposes
+*
+* @author Alexey Aleshin (mailto:al-al-m@yandex.ru)
+* @version $Id$
+* @since 28.02.2017
+*/
+
+package ru.job4j;
