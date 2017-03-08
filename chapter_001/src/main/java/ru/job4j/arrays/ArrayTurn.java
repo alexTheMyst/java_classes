67b2dba776f935b5701844178eb3c2fed6f79263
alexTheMist <alexey.aleshin@yahoo.com>
2017-03-08 10:46:28 +0300

    Fixed turn method.

--- chapter_001/src/main/java/ru/job4j/arrays/ArrayTurn.java
+++ chapter_001/src/main/java/ru/job4j/arrays/ArrayTurn.java
@@ -2,7 +2,6 @@ package ru.job4j.arrays;
 
 /**
  * ArrayTurn calss implements turn square array on 90 degree.
- *
  * @author Alexey Aleshin
  * @version $id$
  * @since 05.03.17
@@ -11,18 +10,22 @@ public class ArrayTurn {
     /**
      * Turn square array on 90 degree.
      * @param givenArray array
-     * @return result array
      */
-    public int[][] turn(int[][] givenArray) {
-        int width = givenArray.length;
-        int heith = givenArray[0].length;
-        int[][] result = new int[heith][width];
-        for (int i = 0; i < heith; ++i) {
-            for (int j = 0; j < width; ++j) {
-                result[i][j] = givenArray[width - j - 1][i];
+    public void turn(int[][] givenArray) {
+        int circlesCount = givenArray.length;
+        for (int i = 0; i < circlesCount / 2; i++) {
+            for (int j = i; j < circlesCount - i - 1; j++) {
+                int temp = givenArray[i][j];
+                //first step
+                givenArray[i][j] = givenArray[circlesCount - 1 - j][i];
+                //second step
+                givenArray[circlesCount - 1 - j][i] = givenArray[circlesCount - 1 - i][circlesCount - 1 - j];
+                //third step
+                givenArray[circlesCount - 1 - i][circlesCount - 1 - j] = givenArray[j][circlesCount - 1 - i];
+                //forth step
+                givenArray[j][circlesCount - 1 - i] = temp;
             }
         }
-        return result;
     }
 }
 
--- chapter_001/src/test/java/ru/job4j/arrays/ArrayTurnTest.java
+++ chapter_001/src/test/java/ru/job4j/arrays/ArrayTurnTest.java
@@ -3,6 +3,7 @@ package ru.job4j.arrays;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.notNullValue;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -27,9 +28,12 @@ public class ArrayTurnTest {
      */
     @Test
     public void testTurnLogic() {
-        final int[][] givenArray = {{1, 5}, {3, 7}};
-        final int[][] testArray = {{3, 1}, {7, 5}};
+        final int[][] givenArray = {{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}, {11, 12, 13, 14, 15}, {16, 17, 18, 19, 20},
+                {21, 22, 23, 24, 25}};
+        final int[][] testArray = {{21, 16, 11, 6, 1}, {22, 17, 12, 7, 2}, {23, 18, 13, 8, 3}, {24, 19, 14, 9, 4},
+                {25, 20, 15, 10, 5}};
         final ArrayTurn arrayTurn = new ArrayTurn();
-        assertThat(arrayTurn.turn(givenArray), is(testArray));
+        arrayTurn.turn(givenArray);
+        assertThat(givenArray, is(testArray));
     }
 }
