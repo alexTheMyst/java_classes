1925bcc01d4e53ef2b9fd325cf6d0443b74202fb
alexTheMist <alexey.aleshin@yahoo.com>
2017-06-15 21:06:44 +0300

    Added sortByNameLength and sortByAllFields methods with tests.

--- chapter_005/src/main/java/ru/job4j/sort/SortUser.java
+++ chapter_005/src/main/java/ru/job4j/sort/SortUser.java
@@ -1,8 +1,6 @@
 package ru.job4j.sort;
 
-import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 /**
  * Contains sort methods implementation for a User class.
@@ -25,4 +23,41 @@ public class SortUser {
         return result;
     }
 
+    /**
+     * Sorts given list of users by name length.
+     *
+     * @param userList list of users
+     * @return sortes list of users
+     */
+    public List<User> sortByNameLength(List<User> userList) {
+        Collections.sort(userList, new Comparator<User>() {
+            @Override
+            public int compare(User o1, User o2) {
+                return o1.getName().length() - o2.getName().length();
+            }
+        });
+        return userList;
+    }
+
+    /**
+     * Sorts given list of users by name length and age.
+     *
+     * @param userList list of users
+     * @return sortes list of users
+     */
+    public List<User> sortByAllFields(List<User> userList) {
+        Collections.sort(userList, new Comparator<User>() {
+            @Override
+            public int compare(User o1, User o2) {
+                int result;
+                if (o1.getName().compareTo(o2.getName()) == 0) {
+                    result = o1.getAge() - o2.getAge();
+                } else {
+                    result = o1.getName().compareTo(o2.getName());
+                }
+                return result;
+            }
+        });
+        return userList;
+    }
 }
--- chapter_005/src/test/java/ru/job4j/sort/SortTests.java
+++ chapter_005/src/test/java/ru/job4j/sort/SortTests.java
@@ -39,6 +39,11 @@ public class SortTests {
     private final User user3 = new User("test3tes", 25);
 
     /**
+     * User variable.
+     */
+    private final User user4 = new User("test3tes", 20);
+
+    /**
      * Creates new SortUser before each test.
      */
     @Before
@@ -59,5 +64,39 @@ public class SortTests {
             System.out.println(u.toString());
         }
         assertThat(this.sortUser.sort(users).toArray()[0], is(user1));
+    }
+
+    /**
+     * Tests that list sorted by name length.
+     */
+    @Test
+    public void testSortByNameLength() {
+        final List<User> unsortedList = new LinkedList<>();
+        final List<User> sortedList = new LinkedList<>();
+        sortedList.add(user1);
+        sortedList.add(user2);
+        sortedList.add(user3);
+        unsortedList.add(user2);
+        unsortedList.add(user3);
+        unsortedList.add(user1);
+        assertThat(this.sortUser.sortByNameLength(unsortedList), is(sortedList));
+    }
+
+    /**
+     * Tests that list sorted by name and by age.
+     */
+    @Test
+    public void testSortByAllFields() {
+        final List<User> unsortedList = new LinkedList<>();
+        final List<User> sortedList = new LinkedList<>();
+        sortedList.add(user1);
+        sortedList.add(user2);
+        sortedList.add(user4);
+        sortedList.add(user3);
+        unsortedList.add(user4);
+        unsortedList.add(user2);
+        unsortedList.add(user3);
+        unsortedList.add(user1);
+        assertThat(this.sortUser.sortByAllFields(unsortedList), is(sortedList));
     }
 }
