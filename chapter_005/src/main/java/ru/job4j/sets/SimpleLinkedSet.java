8b559655d233f2771ca309b65b8f2accefa644ca
alexTheMist <alexey.aleshin@yahoo.com>
2017-09-23 11:07:26 +0300

    Set implementation using linked list. [#24987]

---
+++ chapter_005/src/main/java/ru/job4j/sets/SimpleLinkedSet.java
@@ -0,0 +1,143 @@
+package ru.job4j.sets;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Simple set implementation using linked elements structure.
+ *
+ * @param <E> element type
+ * @author Alexey Aleshin
+ * @version $id$
+ * @since 22.09.17
+ */
+public class SimpleLinkedSet<E> implements Iterable<E> {
+    /**
+     * Link to the first element of list.
+     */
+    private Node<E> firstNode;
+    /**
+     * Link to the last element of list.
+     */
+    private Node<E> lastNode;
+
+    /**
+     * Adds given element to list.
+     *
+     * @param elementValue some element
+     */
+    public void add(E elementValue) {
+        Node<E> newNode = new Node<>(elementValue);
+        if (firstNode == null) {
+            this.firstNode = newNode;
+            this.lastNode = newNode;
+        } else {
+            if (!isCopy(elementValue)) {
+                this.lastNode.next = newNode;
+                this.lastNode = newNode;
+            }
+        }
+    }
+
+    /**
+     * Checks that given element has a copy in list.
+     *
+     * @param elementValue some element
+     * @return true if entity already exists in list or false otherwise
+     */
+    private boolean isCopy(E elementValue) {
+        boolean result = false;
+        Iterator<E> iterator = this.iterator();
+        while (iterator.hasNext()) {
+            E elementFromList = iterator.next();
+            if (elementFromList.equals(elementValue)) {
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Creates and returns iterator.
+     *
+     * @return iterator instance
+     */
+    @Override
+    public Iterator<E> iterator() {
+        return new SimpleLinkedSetIterator();
+    }
+
+    /**
+     * Element of list.
+     *
+     * @param <E> element type
+     */
+    class Node<E> {
+        /**
+         * Value.
+         */
+        private final E value;
+        /**
+         * Next node link.
+         */
+        private Node<E> next;
+
+        /**
+         * Constructor.
+         *
+         * @param value some value
+         */
+        Node(E value) {
+            this.value = value;
+        }
+    }
+
+    /**
+     * Simple iterator implementation.
+     */
+    class SimpleLinkedSetIterator implements Iterator<E> {
+        /**
+         * Link last returned element of list.
+         */
+        private Node<E> nodeToReturn = firstNode;
+
+        /**
+         * Checks that iterator has next element.
+         *
+         * @return true if next element exists or false otherwise
+         */
+        @Override
+        public boolean hasNext() {
+            checkEmptyList();
+            return (this.nodeToReturn != null);
+        }
+
+        /**
+         * Returns next element.
+         *
+         * @return element
+         */
+        @Override
+        public E next() {
+            checkEmptyList();
+            if (this.nodeToReturn == null) {
+                throw new NoSuchElementException();
+            } else {
+                E result = this.nodeToReturn.value;
+                this.nodeToReturn = this.nodeToReturn.next;
+                return result;
+            }
+
+        }
+
+        /**
+         * Checks is list empty.
+         */
+        private void checkEmptyList() {
+            if (firstNode == null) {
+                throw new NoSuchElementException();
+            }
+        }
+    }
+}
---
+++ chapter_005/src/test/java/ru/job4j/sets/SimpleLinkedSetTest.java
@@ -0,0 +1,77 @@
+package ru.job4j.sets;
+
+import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Tests for SimpleLinkedSet.
+ *
+ * @author Alexey Aleshin
+ * @version $id$
+ * @since 22.09.17
+ */
+public class SimpleLinkedSetTest {
+    /**
+     * Adds two equal elements and check that set size is one.
+     */
+    @Test
+    public void whenAddTwoSameEntitiesThenSizeOne() {
+        SimpleLinkedSet<Integer> set = new SimpleLinkedSet<>();
+        int setSize = 0;
+
+        set.add(1);
+        set.add(1);
+        Iterator<Integer> iterator = set.iterator();
+        while (iterator.hasNext()) {
+            iterator.next();
+            setSize++;
+        }
+
+        assertThat(setSize, is(1));
+    }
+
+    /**
+     * Adds three elements and check that after iteration sum is equal.
+     */
+    @Test
+    public void whenAddThreeThenIteratorReturnsThree() {
+        SimpleLinkedSet<Integer> set = new SimpleLinkedSet<>();
+        int sumResult = 0;
+
+        set.add(1);
+        set.add(2);
+        set.add(3);
+        for (Iterator<Integer> iterator = set.iterator(); iterator.hasNext(); ) {
+            sumResult += iterator.next();
+        }
+
+        assertThat(sumResult, is(6));
+    }
+
+    /**
+     * Asks next element from empty set iterator and gets exception.
+     */
+    @Test(expected = NoSuchElementException.class)
+    public void whenAskNextFromSetIteratorThenException() {
+        SimpleLinkedSet<Integer> set = new SimpleLinkedSet<>();
+        set.iterator().next();
+    }
+
+    /**
+     * Adds one element and asks two times and get exception.
+     */
+    @Test(expected = NoSuchElementException.class)
+    public void whenAddOneAndAskTwoFromIteratorThenException() {
+        SimpleLinkedSet<Integer> set = new SimpleLinkedSet<>();
+        set.add(1);
+
+        Iterator<Integer> iterator = set.iterator();
+        iterator.next();
+        iterator.next();
+    }
+}
